eafae260513c4981232c4dfca0837de4399cad15
Hyerim <sinhyerim@gmail.com>
2022-10-12 18:32:23 -0700

    fixed login ui

--- client/src/app/pages/Login.js
+++ client/src/app/pages/Login.js
@@ -1,25 +1,47 @@
 import React from "react";
 import { Box, TextField, Typography, Button } from "@mui/material";
 import "./Login.css";
+import bgImage from "../../assets/images/background_auth.png";
+// import { ThemeProvider, createMuiTheme } from "@mui/material/styles";
 
 const Login = () => {
+  // const theme = createMuiTheme( {
+  //   typography: {
+
+  //   }
+  // })
+
+  const loginPageStyle = {
+    backgroundImage: `url(${bgImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    position: "fixed",
+    height: "100vh",
+    width: "100%",
+    top: 0,
+    left: 0
+  };
+
   return (
-    <div className="Login-component">
+    <div className="Login-component" style={loginPageStyle}>
       <form>
-        <Box className="text-field" sx={{ mt: 10 }}>
+        <Box
+          sx={{ mt: 10 }}
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems="center"
+          justifyContent="center">
           <Typography
-            className="company-name-text"
             variant="h1"
             fontFamily="Kodchasan"
-          >
+            paddingTop={5}
+            color="#bef3ff"
+            textShadow="2px 2p #0c2e53"
+            fontWeight="bold">
             CAHS LIMS
           </Typography>
 
-          <Typography
-            className="system-name-text"
-            fontFamily="Kodchasan"
-            variant="h2"
-          >
+          <Typography variant="h5" color="#7dd8ff" fontFamily="Kodchasan">
             Labotory Information Management System
           </Typography>
         </Box>
@@ -33,8 +55,7 @@ const Login = () => {
           margin="auto"
           marginTop={5}
           padding={3}
-          backgroundColor="rgba(125, 216, 255, 0.8)"
-        >
+          backgroundColor="rgba(125, 216, 255, 0.8)">
           <TextField
             margin="normal"
             type={"email"}
@@ -53,18 +74,16 @@ const Login = () => {
             sx={{ marginTop: 3 }}
             variant="contained"
             backgroundColor="#0B8AC0"
-            style={{ width: 250, height: 50 }}
-          >
+            style={{ width: 250, height: 50 }}>
             Login
           </Button>
 
           <Typography textAligne="center" marginTop={3}>
-            Don't have an anccount?
+            Don&apos;t have an anccount?
           </Typography>
           <Button>Sing Up</Button>
         </Box>
       </form>
-      <div></div>
     </div>
   );
 };
--- client/src/app/utils/theme.js
+++ client/src/app/utils/theme.js
@@ -1,6 +1,7 @@
 import { createTheme } from "@mui/material/styles";
 import MontserratBold from "../../assets/fonts/Montserrat/static/Montserrat-Bold.ttf";
 import Montserrat from "../../assets/fonts/Montserrat/static/Montserrat-Regular.ttf";
+import Kodchasan from "../../assets/fonts/Kodchasan/Kodchasan-Medium.ttf";
 
 const montserrat = {
   fontFamily: "Montserrat",
@@ -26,6 +27,17 @@ const montserratBold = {
   `
 };
 
+const kodchasan = {
+  fontFamily: "Kodchasan",
+  fontStyle: "normal",
+  fontDisplay: "swap",
+  fontWeight: 400,
+  src: `local('Kodchasan'),
+  local('Kodchasan-Medium'),
+  url(${Kodchasan}) format(''woff2)
+  `
+};
+
 export const defaultTheme = createTheme({
   // Color
   primary: {
@@ -46,6 +58,7 @@ export const defaultTheme = createTheme({
   typography: {
     fontFamily: [
       "Montserrat",
+      "Kodchasan",
       "-apple-system",
       "BlinkMacSystemFont",
       '"Segoe UI"',
@@ -61,7 +74,7 @@ export const defaultTheme = createTheme({
   overrides: {
     MuiCssBaseline: {
       "@global": {
-        "@font-face": [montserrat, montserratBold]
+        "@font-face": [montserrat, montserratBold, kodchasan]
       }
     }
   }
