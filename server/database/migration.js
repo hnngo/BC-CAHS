48c621e81651acfd64d589bdf9eb9f2463957e8f
nickluong97@outlook.com <nickluong97@outlook.com>
2022-10-13 14:57:46 -0700

    add user, auth, and user_auth table creation to migration.js

--- server/database/migration.js
+++ server/database/migration.js
@@ -2,16 +2,57 @@ const migrate = (pool) => {
   /**
    * Create table User
    */
-  pool.query(`CREATE TABLE IF NOT EXISTS public.user ( \
-    id SERIAL PRIMARY KEY NOT NULL, \
-    username varchar(45) NOT NULL, \
-    password varchar(450) NOT NULL \
-  )`, (err, res) => {
-    if (!err) {
-      console.log("Created User Table");
+  pool.query(
+    `CREATE TABLE IF NOT EXISTS public.user ( \
+    user_id SERIAL PRIMARY KEY NOT NULL, \
+    username varchar(25) NOT NULL, \
+    password varchar(25) NOT NULL,
+    first_name varchar(25) NOT NULL,
+    last_name varchar(25) NOT NULL \
+  )`,
+    (err, res) => {
+      if (!err) {
+        console.log("Created User Table");
+      }
     }
-  });
+  );
 
+  /**
+   * Create user auth junction/associative table
+   */
+  pool.query(
+    `CREATE TABLE IF NOT EXISTS public.user_auth ( \
+    created_at datetime NOT NULL,
+    user_id int NOT NULL,
+    auth_id int NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES public.user(user_id),
+    FOREIGN KEY (auth_id) REFERENCES public.auth(auth_id)
+  )`,
+    (err, res) => {
+      if (!err) {
+        console.log("Created user_auth table");
+      }
+    }
+  );
+
+  /**
+   * Create auth table
+   */
+
+  pool.query(
+    `CREATE TYPE auth_type AS ENUM ('READ_SAMPLE', 'WRITE_SAMPLE', 'DELETE_SAMPLE')`
+  );
+  pool.query(
+    `CREATE TABLE IF NOT EXISTS public.auth (
+    auth_id SERIAL NOT NULL,
+    auth_type auth_type NOT NULL
+  )`,
+    (err, res) => {
+      if (!err) {
+        console.log("Created auth table");
+      }
+    }
+  );
 
   /**
    * Create table Form
@@ -20,7 +61,6 @@ const migrate = (pool) => {
   /**
    * Create table Form Tracking
    */
-
-}
+};
 
 module.exports = migrate;
