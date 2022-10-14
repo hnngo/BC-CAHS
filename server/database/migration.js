8e8ed0d6072e6247acfa3eb115a0e3714939a2f1
nickluong97@outlook.com <nickluong97@outlook.com>
2022-10-14 01:47:20 -0700

    implemented tables related to form submission. fixed async errors

--- server/database/migration.js
+++ server/database/migration.js
@@ -1,14 +1,14 @@
-const migrate = (pool) => {
+const migrate = async (pool) => {
   /**
    * Create table User
    */
-  pool.query(
+  await pool.query(
     `CREATE TABLE IF NOT EXISTS public.user ( \
-    user_id SERIAL PRIMARY KEY NOT NULL, \
-    username varchar(25) NOT NULL, \
-    password varchar(25) NOT NULL,
-    first_name varchar(25) NOT NULL,
-    last_name varchar(25) NOT NULL \
+    user_id SERIAL PRIMARY KEY NOT NULL UNIQUE, \
+    username VARCHAR(25) NOT NULL, \
+    password VARCHAR(25) NOT NULL,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25) NOT NULL
   )`,
     (err, res) => {
       if (!err) {
@@ -18,11 +18,33 @@ const migrate = (pool) => {
   );
 
   /**
+   * Create auth table
+   */
+
+  await pool.query(
+    `DO $$ BEGIN
+      CREATE TYPE auth_type AS ENUM ('READ_SAMPLE', 'WRITE_SAMPLE', 'DELETE_SAMPLE');
+    EXCEPTION
+      WHEN duplicate_object THEN null;
+    END $$;
+
+    CREATE TABLE IF NOT EXISTS public.auth (
+    auth_id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+    auth_type auth_type NOT NULL
+  )`,
+    (err, res) => {
+      if (!err) {
+        console.log("Created auth table");
+      }
+    }
+  );
+
+  /**
    * Create user auth junction/associative table
    */
-  pool.query(
-    `CREATE TABLE IF NOT EXISTS public.user_auth ( \
-    created_at datetime NOT NULL,
+  await pool.query(
+    `CREATE TABLE IF NOT EXISTS public.user_auth (
+    created_at TIMESTAMP NOT NULL,
     user_id int NOT NULL,
     auth_id int NOT NULL,
     FOREIGN KEY (user_id) REFERENCES public.user(user_id),
@@ -36,31 +58,108 @@ const migrate = (pool) => {
   );
 
   /**
-   * Create auth table
+   * Create submission_details table
    */
 
-  pool.query(
-    `CREATE TYPE auth_type AS ENUM ('READ_SAMPLE', 'WRITE_SAMPLE', 'DELETE_SAMPLE')`
-  );
-  pool.query(
-    `CREATE TABLE IF NOT EXISTS public.auth (
-    auth_id SERIAL NOT NULL,
-    auth_type auth_type NOT NULL
+  // need to handle other
+  await pool.query(
+    `DO $$ BEGIN
+    CREATE TYPE analysis_requested_type AS ENUM ('ATPase', 'Bacteriology', 'Bio-Assay', 'ELISA Cortisol',
+    'ELISA R.sal','PCR', 'Plankton ID', 'RT-qPCR', 'Sea Lice ID', 'Virology', 'Water Analysis');
+    EXCEPTION
+      WHEN duplicate_object THEN null;
+    END $$;
+    
+    CREATE TABLE IF NOT EXISTS public.submission_details (
+    submission_num VARCHAR(20) PRIMARY KEY NOT NULL UNIQUE,
+    company_name VARCHAR(25) NOT NULL,
+    submitter VARCHAR(25) NOT NULL,
+    receipt_date DATE NOT NULL,
+    sampling_time_submission TIMESTAMP NOT NULL,
+    contact_phone_num VARCHAR(15) NOT NULL,
+    purchase_order_num VARCHAR(15) NOT NULL,
+    bc_cahs_receiver_last_name VARCHAR(15) NOT NULL,
+    bc_cahs_receiver_first_name VARCHAR(15) NOT NULL,
+    bc_cahs_custodian_initials VARCHAR(5) NOT NULL,
+    client_case_num INT NOT NULL,
+    sampling_date TIMESTAMP NOT NULL,
+    bc_cahs_p_i VARCHAR(5) NOT NULL,
+    sampling_location VARCHAR(50) NOT NULL,
+    bc_cahs_project VARCHAR(30) NOT NULL,
+    initial_placement VARCHAR(20) NOT NULL,
+    analysis_requested analysis_requested_type NOT NULL
   )`,
     (err, res) => {
       if (!err) {
-        console.log("Created auth table");
+        console.log("Created submission_details table");
       }
     }
   );
 
   /**
-   * Create table Form
+   * Create sample_details
    */
+  await pool.query(
+    `DO $$ BEGIN
+    CREATE TYPE sample_condition AS ENUM ('Dry Ice', 'Frozen', 'Ice Packs', 'Thawed', 'RT', 'Other');
+    EXCEPTION
+      WHEN duplicate_object THEN null;
+    END $$;
 
-  /**
-   * Create table Form Tracking
-   */
+    DO $$ BEGIN
+    CREATE TYPE sample_type AS ENUM ('Wild', 'Brood Stock', 'Freshwater', 'Saltwater', 'Other');
+    EXCEPTION
+      WHEN duplicate_object THEN null;
+    END $$;
+
+    CREATE TABLE IF NOT EXISTS public.sample_details (
+      sample_id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+      num_of_samples SMALLINT NOT NULL,
+      species VARCHAR(25) NOT NULL,
+      other_details VARCHAR(255),
+      sample_condition sample_condition NOT NULL,
+      sample_type sample_type NOT NULL
+    )`,
+    (err, res) => {
+      if (!err) {
+        console.log("Created sample_details table");
+      }
+    }
+  );
+
+  await pool.query(
+    `DO $$ BEGIN 
+    CREATE TYPE rt_qpcr_type AS ENUM ('IHNv', 'IPNv', 'ISAv', 'VHSv', 'PRV-L1', 'A.sal', 'P.sal', 'R.sal', 'ELFa', 'N.perurans');
+    EXCEPTION
+      WHEN duplicate_object THEN null;
+    END $$;                                                                                   
+
+    CREATE TABLE IF NOT EXISTS public.rt_qpcr_targets (
+      rt_qpcr_id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+      rt_qpcr_type rt_qpcr_type NOT NULL
+    )`,
+    (err, res) => {
+      if (!err) {
+        console.log("Created rt_qpcr_targets table");
+      }
+    }
+  );
+
+  await pool.query(
+    `                                                                             
+    CREATE TABLE IF NOT EXISTS public.submission_rt_qpcr (
+    other_description VARCHAR(255),
+    rt_qpcr_id INT NOT NULL,
+    sample_id INT NOT NULL,
+    FOREIGN KEY (rt_qpcr_id) REFERENCES public.rt_qpcr_targets(rt_qpcr_id),
+    FOREIGN KEY (sample_id) REFERENCES public.sample_details(sample_id)
+    )`,
+    (err, res) => {
+      if (!err) {
+        console.log("Created submission_rt_qpcr table");
+      }
+    }
+  );
 };
 
 module.exports = migrate;
