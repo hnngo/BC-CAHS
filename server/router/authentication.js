f35acf0963aad0d63ab0d51d0a5398b8f55de866
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-10-14 11:33:34 -0700

    implement signup route

--- server/router/authentication.js
+++ server/router/authentication.js
@@ -29,7 +29,6 @@ router.post("/login", (req, res) => {
 router.use(bodyParser.json())
 router.post("/signup", async (req, res) => {
   let { username, password, passwordConfirm } = req.body;
-  console.log({ username, password, passwordConfirm });
   let validation = true;
 
   if (!username || !password) {
@@ -50,18 +49,44 @@ router.post("/signup", async (req, res) => {
   if (validation) {
     let hashedPw = await bcrypt.hash(password, 10);
     console.log(hashedPw);
+    // res.send({ msg: "this is a signup success message" });
 
-    res.send({ msg: "this is a signup success message" });
-    // pool.query("inserting user to db logic here")
+    pool.query(
+      `SELECT * FROM public.user
+      WHERE username = $1`, [username], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result.rows);
+        if (result.rows.length > 0) {
+          res.send({ errMsg: "Username is already registered. Please provide another." });
+        } else {
+          pool.query(
+            `INSERT INTO public.user (username, password)
+            VALUES ($1, $2)
+            RETURNING id, password`, [username, hashedPw], (err, result) => {
+            if (err) {
+              res.send({ errMsg: "error" });
+            } else {
+              res.send({ msg: "Signup complete. Please log in using your login credentials." })
+            }
+          }
+          )
+        }
+      }
+    }
+    );
+
   }
+
 });
 
 /**
  * Log out
  */
 router.post("/logout", (req, res) => {
-  // Hyunbae area
-  res.send("");
+  req.logout();
+  res.render("/authentication", { msg: "You have been logged out." });
 });
 
 module.exports = router;
