0a17f2180cf1a230657d3a3fcf7bae9afa209880
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2022-10-26 15:47:20 -0700

    [FE] Add handle click for action form

--- client/src/app/pages/Home/Sample/ManageSample.js
+++ client/src/app/pages/Home/Sample/ManageSample.js
@@ -2,25 +2,66 @@ import React from "react";
 
 // Components
 import { DataGrid } from "@mui/x-data-grid";
-import { Chip, Stack } from "@mui/material";
+import { Chip, Grid, Stack, Modal, Box, Typography } from "@mui/material";
 import { styled, useTheme } from "@mui/material/styles";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import EditIcon from "@mui/icons-material/Edit";
 
 // Utils
 import { getMockFormData } from "../../../../mocks/mock-sample";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4
+};
+
 const ManageSample = () => {
   const theme = useTheme();
+  const [data, setData] = React.useState(getMockFormData(100));
   const [columns, setColumns] = React.useState([]);
+  const [openFormDetail, setOpenFormDetail] = React.useState(false);
 
-  // Using Mockdata for now
-  const data = getMockFormData(100);
+  const handleOpen = () => setOpenFormDetail(true);
+  const handleClose = () => setOpenFormDetail(false);
 
   React.useEffect(() => {
     const col = [
+      {
+        field: "action",
+        headerName: "Action",
+        width: 120,
+        renderCell: (params) => {
+          return (
+            <Stack direction="row" flexWrap={"wrap"}>
+              <Chip
+                icon={<EditIcon sx={{ fill: theme.primary.white, fontSize: "16px" }} />}
+                label={"Edit"}
+                sx={{
+                  backgroundColor: theme.primary.dark,
+                  color: "#fff",
+                  padding: 1,
+                  cursor: "pointer"
+                }}
+                variant="filled"
+                onClick={() => {
+                  console.log("aaa");
+                  handleOpen();
+                }}
+              />
+            </Stack>
+          );
+        }
+      },
       { field: "id", headerName: "Submission #", width: 120 },
       { field: "receipt_date", headerName: "Date Received", width: 140 },
       { field: "sampling_time_submission", headerName: "Time Received", width: 120 },
@@ -70,41 +111,55 @@ const ManageSample = () => {
   }, []);
 
   return (
-    <DataGrid
-      components={{
-        ColumnMenuIcon: styled(MoreVertIcon)({
-          fill: theme.primary.white
-        }),
-        ColumnSortedAscendingIcon: styled(ArrowUpwardIcon)({
-          fill: theme.primary.white
-        }),
-        ColumnSortedDescendingIcon: styled(ArrowDownwardIcon)({
-          fill: theme.primary.white
-        }),
-        ColumnFilteredIcon: styled(FilterAltIcon)({
-          fill: theme.primary.white
-        })
-      }}
-      sx={{
-        "& .sample-form-table-header": {
-          backgroundColor: theme.primary.dark,
-          color: theme.primary.white
-        },
-        "& .sample-form-table-row": {
-          backgroundColor: theme.primary.standard
-        },
-        "&:hover": {
-          cursor: "pointer"
-        }
-      }}
-      rows={data}
-      columns={columns}
-      pageSize={20}
-      rowsPerPageOptions={[20]}
-      getRowHeight={() => "auto"}
-      getRowClassName={() => "sample-form-table-row"}
-      Co
-    />
+    <Grid height={"100%"}>
+      <DataGrid
+        components={{
+          ColumnMenuIcon: styled(MoreVertIcon)({
+            fill: theme.primary.white
+          }),
+          ColumnSortedAscendingIcon: styled(ArrowUpwardIcon)({
+            fill: theme.primary.white
+          }),
+          ColumnSortedDescendingIcon: styled(ArrowDownwardIcon)({
+            fill: theme.primary.white
+          }),
+          ColumnFilteredIcon: styled(FilterAltIcon)({
+            fill: theme.primary.white
+          })
+        }}
+        sx={{
+          "& .sample-form-table-header": {
+            backgroundColor: theme.primary.dark,
+            color: theme.primary.white
+          },
+          "& .sample-form-table-row": {
+            backgroundColor: theme.primary.standard
+          },
+          "&:hover": {
+            cursor: "pointer"
+          }
+        }}
+        rows={data}
+        columns={columns}
+        onRowClick={() => {
+          // handleOpen();
+        }}
+        pageSize={20}
+        rowsPerPageOptions={[20]}
+        getRowHeight={() => "auto"}
+        getRowClassName={() => "sample-form-table-row"}
+      />
+      <Modal open={openFormDetail} onClose={handleClose}>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Form Detail
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </Grid>
   );
 };
 
