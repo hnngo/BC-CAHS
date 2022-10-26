cfe96735873078fbf5e36fffb2f15bcf6803d562
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2022-10-26 15:23:47 -0700

    [FE] Add form table

--- client/src/app/pages/Home/Sample/ManageSample.js
+++ client/src/app/pages/Home/Sample/ManageSample.js
@@ -1,13 +1,111 @@
 import React from "react";
 
+// Components
+import { DataGrid } from "@mui/x-data-grid";
+import { Chip, Stack } from "@mui/material";
+import { styled, useTheme } from "@mui/material/styles";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+
 // Utils
 import { getMockFormData } from "../../../../mocks/mock-sample";
 
 const ManageSample = () => {
-  const data = getMockFormData(10);
-  console.log(data);
+  const theme = useTheme();
+  const [columns, setColumns] = React.useState([]);
 
-  return <div>ManageSample</div>;
+  // Using Mockdata for now
+  const data = getMockFormData(100);
+
+  React.useEffect(() => {
+    const col = [
+      { field: "id", headerName: "Submission #", width: 120 },
+      { field: "receipt_date", headerName: "Date Received", width: 140 },
+      { field: "sampling_time_submission", headerName: "Time Received", width: 120 },
+      { field: "bc_cahs_receiver_first_name", headerName: "Receiver First Name", width: 150 },
+      { field: "bc_cahs_receiver_last_name", headerName: "Receiver Last Name", width: 150 },
+      { field: "company_name", headerName: "Company", width: 100 },
+      { field: "submitter", headerName: "Submitter" },
+      { field: "contact_phone_num", headerName: "Contact Phone #", width: 130 },
+      { field: "client_case_num", headerName: "Client Case #", width: 110 },
+      { field: "sampling_date", headerName: "Sampling Date", width: 120 },
+      { field: "sampling_location", headerName: "Sampling Location", width: 140 },
+      { field: "bc_cahs_custodian_initials", headerName: "BC CAHS Custodian", width: 160 },
+      { field: "bc_cahs_p_i", headerName: "BC CAHS P.I.", width: 120 },
+      { field: "bc_cahs_project", headerName: "BC CAHS Projcet", width: 150 },
+      { field: "num_of_samples", headerName: "# of Sample", width: 120 },
+      { field: "species", headerName: "Sample Species", width: 150 },
+      { field: "sample_type", headerName: "Sample Type", width: 120 },
+      { field: "sample_origin", headerName: "Sample Origin", width: 130 },
+      { field: "sample_condition", headerName: "Sample Condition", width: 150 },
+      { field: "other_details", headerName: "Sample Details", width: 140 },
+
+      { field: "analysis_requested", headerName: "Analysis Requested", width: 140 },
+      {
+        field: "rt_qpcr_type",
+        headerName: "RT-qPCR Targets",
+        width: 200,
+        renderCell: (params) => {
+          return (
+            <Stack direction="row" flexWrap={"wrap"}>
+              {(params.value || []).map((v) => (
+                <Chip
+                  key={v}
+                  label={v}
+                  sx={{ backgroundColor: theme.primary.dark, color: "#fff", margin: 0.5 }}
+                  variant="filled"
+                />
+              ))}
+            </Stack>
+          );
+        }
+      }
+    ];
+    col.forEach((c) => {
+      c.headerClassName = "sample-form-table-header";
+    });
+    setColumns(col);
+  }, []);
+
+  return (
+    <DataGrid
+      components={{
+        ColumnMenuIcon: styled(MoreVertIcon)({
+          fill: theme.primary.white
+        }),
+        ColumnSortedAscendingIcon: styled(ArrowUpwardIcon)({
+          fill: theme.primary.white
+        }),
+        ColumnSortedDescendingIcon: styled(ArrowDownwardIcon)({
+          fill: theme.primary.white
+        }),
+        ColumnFilteredIcon: styled(FilterAltIcon)({
+          fill: theme.primary.white
+        })
+      }}
+      sx={{
+        "& .sample-form-table-header": {
+          backgroundColor: theme.primary.dark,
+          color: theme.primary.white
+        },
+        "& .sample-form-table-row": {
+          backgroundColor: theme.primary.standard
+        },
+        "&:hover": {
+          cursor: "pointer"
+        }
+      }}
+      rows={data}
+      columns={columns}
+      pageSize={20}
+      rowsPerPageOptions={[20]}
+      getRowHeight={() => "auto"}
+      getRowClassName={() => "sample-form-table-row"}
+      Co
+    />
+  );
 };
 
 export default ManageSample;
--- client/src/mocks/mock-sample.js
+++ client/src/mocks/mock-sample.js
@@ -4,9 +4,10 @@ export const getMockFormData = (numberOfForm = 1) => {
   const data = [];
   for (let i = 0; i < numberOfForm; i++) {
     data.push({
-      submission_num: faker.datatype.uuid(),
-      receipt_date: faker.date.past(),
-      sampling_time_submission: faker.date.past(),
+      id: faker.random.alpha(8).toUpperCase(),
+      submission_num: faker.random.alpha(8).toUpperCase(),
+      receipt_date: new Date(faker.date.past()).toDateString(),
+      sampling_time_submission: new Date(faker.date.past()).toLocaleTimeString(),
       bc_cahs_receiver_last_name: faker.name.lastName(),
       bc_cahs_receiver_first_name: faker.name.firstName(),
       company_name: faker.company.name(),
@@ -14,10 +15,10 @@ export const getMockFormData = (numberOfForm = 1) => {
       contact_phone_num: faker.phone.number(),
       purchase_order_num: faker.datatype.string(10).toUpperCase(),
       client_case_num: faker.random.numeric(5),
-      sampling_date: faker.date.future(),
+      sampling_date: new Date(faker.date.future()).toDateString(),
       sampling_location: faker.address.cityName(),
       bc_cahs_custodian_initials: faker.name.firstName(),
-      bc_cahs_p_i: faker.random.alpha(5),
+      bc_cahs_p_i: faker.random.alpha(5).toUpperCase(),
       bc_cahs_project: faker.commerce.productName(),
 
       num_of_samples: faker.datatype.number({ min: 1, max: 50 }),
@@ -53,7 +54,11 @@ export const getMockFormData = (numberOfForm = 1) => {
         "Virology",
         "Water Analysis"
       ]),
-      rt_qpcr_type: ["IHNv", "IPNv", "ISAv", "VHSv"]
+
+      rt_qpcr_type: faker.helpers.arrayElements(
+        ["IHNv", "IPNv", "ISAv", "VHSv", "PRV-L1", "A.sal", "P.sal", "R.sal", "ELFa", "N.perurans"],
+        faker.datatype.number({ min: 1, max: 4 })
+      )
     });
   }
   return data;
